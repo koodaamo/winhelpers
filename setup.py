2c16d88702f58efe6059a298bdad731adb09af8d
Petri Savolainen <petri.savolainen@koodaamo.fi>
2016-09-20 18:55:50 +0300

    correct pywin32 requirement

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '1.2'
+version = '1.3'
 
 setup(name='winhelpers',
       version=version,
@@ -22,5 +22,5 @@ setup(name='winhelpers',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
-      install_requires=["pywin32",],
+      install_requires=["pypiwin32",],
       )
