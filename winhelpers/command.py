83b15d0a5d947952edbfcd449160b91f96a2823b
Petri Savolainen <petri.savolainen@koodaamo.fi>
2016-10-19 18:36:14 +0300

    support trying a custom Dummy WAMP service in winhelper test client

--- winhelpers/command.py
+++ winhelpers/command.py
@@ -1,11 +1,11 @@
 import sys
 import win32serviceutil
-from .service import BaseService, BaseAsyncioService, WAMPComponentService
+from .service import BaseService, BaseAsyncioService, DummyWAMPService
 
 
 def winhelper():
    if len(sys.argv) < 3:
-      print("syntax: service [install/remove/start/stop] [BaseService/BaseAsyncioService/WAMPComponentService]")
+      print("syntax: winhelper [install/remove/start/stop] [BaseService/BaseAsyncioService/DummyWAMPService]")
       sys.exit()
    srvklass = sys.argv.pop()
    win32serviceutil.HandleCommandLine(eval(srvklass))
