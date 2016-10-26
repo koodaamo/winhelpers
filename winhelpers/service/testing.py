00aa4adde009cc9da78ec404c30395f30a1901cd
Petri Savolainen <petri.savolainen@koodaamo.fi>
2016-10-26 11:01:05 +0300

    improve logging & test router configurability using environment

--- winhelpers/service/testing.py
+++ winhelpers/service/testing.py
@@ -1,10 +1,15 @@
 """TEST SERVERS"""
 
-import os, asyncio
-import servicemanager
+import os, asyncio, sys
+from servicemanager import LogInfoMsg, LogErrorMsg
 from autobahn.asyncio.wamp import ApplicationSession
 
 from . import *
+
+def log_exception(exctype, value, tb):
+   LogErrorMsg("%s (%s): %s" % (exctype, value, tb))
+
+sys.excepthook = log_exception
 
 
 class DummyService(BaseService):
@@ -32,18 +37,23 @@ class DummyTransportProtocol(asyncio.Protocol):
 class DummyAsyncioTransportService(BaseAsyncioTransportService):
    ""
 
+   _svc_name_ = "DummyAsyncioTransportService"
+   _svc_display_name_ = "Dummy Asyncio transport test service"
+   _svc_description_ = "Dummy Asyncio transport service used in winhelpers testing"
+
    def _transport_connector(self):
       "return the transport connector coroutine"
       HOST = os.environ.get("TEST_HOST")
       PORT = os.environ.get("TEST_PORT")
+      assert (HOST and PORT), "TEST_HOST or TEST_PORT variable not in environment: %s" % ', '.join(os.environ.keys())
       return self._loop.create_connection(DummyTransportProtocol, host=HOST, port=PORT)
 
 
 class DummyWAMPComponent(ApplicationSession):
    ""
 
-   async def onJoin(self, details):
-      servicemanager.LogInfoMsg("%s: connection made" % self.__class__.__name__)
+   def onJoin(self, details):
+      LogInfoMsg("%s: joined realm" % self.__class__.__name__)
 
 
 class DummyWAMPService(BaseWAMPComponentService):
@@ -54,7 +64,9 @@ class DummyWAMPService(BaseWAMPComponentService):
 
    @property
    def wmp_url(self):
-      return "ws://%s/ws" % os.environ.get("TEST_ROUTER")
+      url = os.environ.get("TEST_ROUTER_URL")
+      LogInfoMsg("%s: using ws URL '%s'" % (self.__class__.__name__, url))
+      return url
 
    wmp_realm = "realm1"
    wmp_sessioncomponent = DummyWAMPComponent
