1f06e7c754d332ba29769351d793c53c284fc0eb
Petri Savolainen <petri.savolainen@koodaamo.fi>
2017-01-20 15:38:19 +0200

    add more docs to registry module

--- winhelpers/registry.py
+++ winhelpers/registry.py
@@ -2,8 +2,16 @@ import winreg
 import win32process
 #from .util import is32bit_py, is64bit_os
 
-# Windows registry access is foobar when 32bit programs run on 64bit OS
+# Windows registry access is funny when 32bit programs run on 64bit OS; there is a
+# different registry 'view' depending on the bitness.. for the ugly background,
+# see https://msdn.microsoft.com/en-us/library/windows/desktop/aa384253.aspx
+
+# Basically, we need to detect if this this 32bit app on 64bit OS
+# see https://msdn.microsoft.com/en-us/library/windows/desktop/ms684139.aspx
 wow64 = win32process.IsWow64Process()
+
+# Depending on the result, need to set right keys to get the registry view we want,
+# ie. on 64bit OS, get the 64bit 'view' always, even if the app is 32bit
 rflag = (winreg.KEY_READ | winreg.KEY_WOW64_64KEY) if wow64 else winreg.KEY_READ
 wflag =  winreg.KEY_WRITE | winreg.KEY_SET_VALUE
 wflag = (wflag | winreg.KEY_WOW64_64KEY) if wow64 else wflag
@@ -30,7 +38,7 @@ def get_registry_settings(rpath):
 
 def store_to_registry(data, rkey, rpath):
 
-   # open existing or create new key
+   # open existing or create new key (using the correct registry mode/view!)
    reg = winreg.CreateKeyEx(rkey, rpath, access=wflag)
 
    for dk, dv in data.items():
