fefc84b69e0a9d86489d2b778514797fb3df82a0
Petri Savolainen <petri.savolainen@koodaamo.fi>
2017-01-20 15:17:34 +0200

    fix the fixture: note that it's the parameter(name)s that are parametrized, not the fixtures

--- winhelpers/tests/fixtures.py
+++ winhelpers/tests/fixtures.py
@@ -1,22 +1,33 @@
-from pytest import raises, fixture
+import sys
+from pytest import fixture
 import win32service, win32serviceutil
 
 
-@fixture(scope="function")
-def installed(request):
-   srv_klass = request.param
+@fixture
+def installed(srv_klass):
+   "Provide instllation and removal of Windows Service class"
+
+   # install
    sys.argv = sys.argv[:1] + ["install"]
    win32serviceutil.HandleCommandLine(srv_klass)
+
+   # wait for install to complete
    while True:
       try:
          status_code = win32serviceutil.QueryServiceStatus(srv_klass._svc_name_)[1]
          break
-      except:
+      except Exception as exc:
          pass
+
+   # return the installed class
    yield srv_klass
+
+   # wait for the service to stop
    while True:
       status_code = win32serviceutil.QueryServiceStatus(srv_klass._svc_name_)[1]
       if status_code == win32service.SERVICE_STOPPED:
          break
+
+   # remove the service
    sys.argv = sys.argv[:1] + ["remove"]
    win32serviceutil.HandleCommandLine(srv_klass)
