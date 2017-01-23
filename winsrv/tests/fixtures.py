10b91d993139440ecd258dba0ad1c402041fb60f
Petri Savolainen <petri.savolainen@koodaamo.fi>
2017-01-23 15:29:54 +0200

    simplify; use the new control (static) methods in the fixture

--- winsrv/tests/fixtures.py
+++ winsrv/tests/fixtures.py
@@ -4,30 +4,12 @@ import win32service, win32serviceutil
 
 
 @fixture
-def installed(srv_klass):
+def installed(service):
    "Provide instllation and removal of Windows Service class"
 
-   # install
-   sys.argv = sys.argv[:1] + ["install"]
-   win32serviceutil.HandleCommandLine(srv_klass)
-
-   # wait for install to complete
-   while True:
-      try:
-         status_code = win32serviceutil.QueryServiceStatus(srv_klass._svc_name_)[1]
-         break
-      except Exception as exc:
-         pass
+   service.install_service()
 
    # return the installed class
-   yield srv_klass
+   yield service
 
-   # wait for the service to stop
-   while True:
-      status_code = win32serviceutil.QueryServiceStatus(srv_klass._svc_name_)[1]
-      if status_code == win32service.SERVICE_STOPPED:
-         break
-
-   # remove the service
-   sys.argv = sys.argv[:1] + ["remove"]
-   win32serviceutil.HandleCommandLine(srv_klass)
+   service.remove_service()
